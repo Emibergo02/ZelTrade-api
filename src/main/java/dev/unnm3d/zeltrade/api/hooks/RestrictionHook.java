01a8982bb18ee46b332dc4c7fa570f857f12caf3
Emibergo02 <36164338+Emibergo02@users.noreply.github.com>
2026-02-10 10:09:53 +0100

    Update javadoc

--- src/main/java/dev/unnm3d/zeltrade/api/hooks/RestrictionHook.java
+++ src/main/java/dev/unnm3d/zeltrade/api/hooks/RestrictionHook.java
@@ -7,5 +7,11 @@ public interface RestrictionHook<T extends ITrade> {
 
     String getName();
 
+    /**
+     * Checks if the given player is restricted from engaging in the specified trade.
+     * @param player The player to check for restrictions.
+     * @param trade The trade to check against.
+     * @return true if the player is restricted from the trade, false otherwise.
+     */
     boolean restrict(Player player, T trade);
 }
